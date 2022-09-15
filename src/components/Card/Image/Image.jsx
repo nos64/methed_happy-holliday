637bf485f6caf74197d5648e3740b9524b980119
Mikhail Nosov <manosov1984@gmail.com>
2022-09-16 02:20:11 +0400

    feat: add bg-image

--- src/components/Card/Image/Image.jsx
+++ src/components/Card/Image/Image.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Image.module.css';
 import Felicitation from './Felicitation/Felicitation';
+import imageCard from '../../../img/card-bg.jpg';
 
 const Image = () => {
   return (
     <div className={style.image}>
-      <img className={style.img} src="../../../img/card-bg.jpg" alt="" />
+      <img className={style.img} src={imageCard} alt="" />
       <Felicitation />
     </div>
   );
